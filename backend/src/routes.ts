729ec61f12deb5548cf696141d44183ccef5529d
lucasapnobrega <lucasapnobrega@gmail.com>
2024-11-01 23:24:02 -0300

    feat: adicionando cadastro e listagem de categorias

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,13 +4,18 @@ import { CreateUserController } from './controllers/user/CreateUserController'
 import { AuthUserController } from './controllers/user/AuthUserController'
 import { DetailUserController } from './controllers/user/DetailUserController'
 import { isAuthenticated } from './middlewares/isAuthenticated'
+import { CreateCategoryController } from './controllers/category/CreateCategoryController'
+import { ListCategoriesController } from './controllers/category/ListCategoriesController'
 
 const router = Router()
 
+// user
 router.post("/users", new CreateUserController().handle)
-
 router.post("/login", new AuthUserController().handle)
-
 router.get("/userinfo", isAuthenticated, new DetailUserController().handle)
+
+// category
+router.post("/category", isAuthenticated, new CreateCategoryController().handle)
+router.get("/category", isAuthenticated, new ListCategoriesController().handle)
 
 export { router }
