efc2a58eb2ddce8f9f4ca154ba2fc718c96abac1
lucasapnobrega <lucasapnobrega@gmail.com>
2024-11-02 21:08:46 -0300

    adicionando cadastro de produtos e tratando o upload de imagens com multer

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,13 +1,19 @@
 import { Router } from 'express'
 
+import multer from 'multer'
+import uploadConfig from './config/multer'
+
 import { CreateUserController } from './controllers/user/CreateUserController'
 import { AuthUserController } from './controllers/user/AuthUserController'
 import { DetailUserController } from './controllers/user/DetailUserController'
 import { isAuthenticated } from './middlewares/isAuthenticated'
 import { CreateCategoryController } from './controllers/category/CreateCategoryController'
 import { ListCategoriesController } from './controllers/category/ListCategoriesController'
+import { CreateProductController } from './controllers/product/CreateProductController'
 
 const router = Router()
+
+const upload = multer(uploadConfig.upload("./temp"))
 
 // user
 router.post("/users", new CreateUserController().handle)
@@ -17,5 +23,8 @@ router.get("/userinfo", isAuthenticated, new DetailUserController().handle)
 // category
 router.post("/category", isAuthenticated, new CreateCategoryController().handle)
 router.get("/category", isAuthenticated, new ListCategoriesController().handle)
+
+// product
+router.post("/product", isAuthenticated, upload.single('file'), new CreateProductController().handle)
 
 export { router }
